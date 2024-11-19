761a898ec71e56a5a42a8c36fffd392986f08fc4
Sohail makandar <sohailmakandar55@gmail.com>
2024-11-19 19:13:32 +0530

    Add files via upload
    Landing Page Project

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change Navbar Style on Scroll
+window.addEventListener('scroll', () => {
+     const navbar = document.querySelector('.navbar');
+     if (window.scrollY > 50) {
+       navbar.classList.add('scrolled');
+     } else {
+       navbar.classList.remove('scrolled');
+     }
+   });
+   
